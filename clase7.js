cfb39d3ef6898f36e0bb46fa9e06c4c4c3f80325
Diegobonahora <diegobonahora12@gmial.com>
2025-06-10 11:57:06 -0300

    clase 7

---
+++ clase7.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const precioElemento = document.getElementById("precioProducto");
+    precioElemento.innerText = "Precio: $450";
+});
